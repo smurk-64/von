59e8ec774528abf2fe4c16e3b2853f8f7d970582
smurk-64 <silvertosh24@gmail.com>
2025-06-21 20:02:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,26 +17,18 @@ return new Promise((resolve) => {
 rl.question(text, resolve)
 })}
 
-
 async function startSesi() {
 const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
-const { state, saveCreds } = await useMultiFileAuthState(`./session`)
+const { state, saveCreds } = await useMultiFileAuthState(./session)
 const { version, isLatest } = await fetchLatestBaileysVersion()
 
-
-console.log(chalk.blue.bold(`
-WELCOME TO KINGVON MD
-${chalk.yellow.bold(`
-VERSION : 1.0. `)}
-${chalk.magenta.bold(`
-MODE BRUTAL`)}
-${chalk.green.bold("📃 Information :")}   
-${chalk.red.bold(`
+console.log(chalk.blue.bold(   WELCOME TO KINGVON MD   ${chalk.yellow.bold(
+VERSION : 1.0. )}   ${chalk.magenta.bold(
+MODE BRUTAL)}   ${chalk.green.bold("📃 Information :")}      ${chalk.red.bold(
 ▢ Script : KINGVON MD
 ▢ Author : KINGVON
 ▢ Versi : 1.0
-`)}
-${chalk.white.italic("Created By KINGVON")}\n`));
+)}   ${chalk.white.italic("Created By KINGVON")}\n));
 
 const connectionOptions = {
 version,
@@ -59,7 +51,7 @@ const lubyz = func.makeWASocket(connectionOptions)
 if (usePairingCode && !lubyz.authState.creds.registered) {
 const phoneNumber = await question(color('NUMBER FOR PAIRING EXAMPLE 254720326316 :\n', 'green'));
 const code = await lubyz.requestPairingCode(phoneNumber.trim())
-console.log(`${chalk.redBright('Your Pairing Code')} : ${code}`)
+console.log(${chalk.redBright('Your Pairing Code')} : ${code})
 }
 store?.bind(lubyz.ev)
 
@@ -71,7 +63,7 @@ console.log(color(lastDisconnect.error, 'deeppink'))
 if (lastDisconnect.error == 'Error: Stream Errored (unknown)') {
 process.exit()
 } else if (reason === DisconnectReason.badSession) {
-console.log(color(`Bad Session File, Please Delete Session and Scan Again`))
+console.log(color(Bad Session File, Please Delete Session and Scan Again))
 process.exit()
 } else if (reason === DisconnectReason.connectionClosed) {
 console.log(color('[SYSTEM]', 'white'), color('Connection closed, reconnecting...', 'deeppink'))
@@ -83,7 +75,7 @@ process.exit()
 console.log(color('Connection Replaced, Another New Session Opened, Please Close Current Session First'))
 lubyz.logout()
 } else if (reason === DisconnectReason.loggedOut) {
-console.log(color(`Device Logged Out, Please Scan Again And Run.`))
+console.log(color(Device Logged Out, Please Scan Again And Run.))
 lubyz.logout()
 } else if (reason === DisconnectReason.restartRequired) {
 console.log(color('Restart Required, Restarting...'))
@@ -98,22 +90,20 @@ console.log(color('Menghubungkan . . . '))
 let teksnotif = `
 *KINGVON MD connected *
 
- *▢ Botname :* ${global.namabot2}
- *▢ Version :* 1.0
- *▢ Name : ${lubyz.user.id.split(":")[0]}*`
+▢ Botname : ${global.namabot2}
+▢ Version : 1.0
+▢ Name : ${lubyz.user.id.split(":")[0]}`
 lubyz.sendMessage( global.owner+"@s.whatsapp.net", {text: teksnotif})
 console.log(color('Bot Berhasil Tersambung'))
 }
 })
-
-
 
 lubyz.ev.on('call', async (user) => {
 if (!global.anticall) return
 for (let ff of user) {
 if (ff.isGroup == false) {
 if (ff.status == "offer") {
-let sendcall = await lubyz.sendMessage(ff.from, {text: `@${ff.from.split("@")[0]} Maaf Kamu Akan Saya Block Karna Ownerbot Menyalakan Fitur *Anticall*\nJika Tidak Sengaja Segera Hubungi Owner Untuk Membuka Blokiran Ini`, contextInfo: {mentionedJid: [ff.from], externalAdReply: {showAdAttribution: true, thumbnail: fs.readFileSync("./media/warning.jpg"), title: "｢ CALL DETECTED ｣", previewType: "PHOTO"}}}, {quoted: null})
+let sendcall = await lubyz.sendMessage(ff.from, {text: @${ff.from.split("@")[0]} Maaf Kamu Akan Saya Block Karna Ownerbot Menyalakan Fitur *Anticall*\nJika Tidak Sengaja Segera Hubungi Owner Untuk Membuka Blokiran Ini, contextInfo: {mentionedJid: [ff.from], externalAdReply: {showAdAttribution: true, thumbnail: fs.readFileSync("./media/warning.jpg"), title: "｢ CALL DETECTED ｣", previewType: "PHOTO"}}}, {quoted: null})
 lubyz.sendContact(ff.from, [owner], "Developer WhatsApp Bot", sendcall)
 await sleep(10000)
 await lubyz.updateBlockStatus(ff.from, "block")
@@ -153,22 +143,22 @@ ppuser = await lubyz.profilePictureUrl(num, 'image')
 ppuser = 'https://files.catbox.moe/mtvyj5.jpg'
 }
 if (anu.action == 'add') {
-lubyz.sendMessage(anu.id, {text: check ? `@${anu.author.split("@")[0]} Telah Menambahkan @${num.split("@")[0]} Ke Dalam Grup Ini` : `Hallo Kak @${num.split("@")[0]} Selamat Datang Di *${namagc}*`, 
+lubyz.sendMessage(anu.id, {text: check ? @${anu.author.split("@")[0]} Telah Menambahkan @${num.split("@")[0]} Ke Dalam Grup Ini : Hallo Kak @${num.split("@")[0]} Selamat Datang Di *${namagc}*,
 contextInfo: {mentionedJid: [...tag], externalAdReply: { thumbnailUrl: ppuser, title: '© Welcome Message', body: '', renderLargerThumbnail: true, sourceUrl: linkgc, mediaType: 1}}})
-} 
-if (anu.action == 'remove') { 
-lubyz.sendMessage(anu.id, {text: check ? `@${anu.author.split("@")[0]} Telah Mengeluarkan @${num.split("@")[0]} Dari Grup Ini` : `@${num.split("@")[0]} Telah Keluar Dari Grup Ini`, 
+}
+if (anu.action == 'remove') {
+lubyz.sendMessage(anu.id, {text: check ? @${anu.author.split("@")[0]} Telah Mengeluarkan @${num.split("@")[0]} Dari Grup Ini : @${num.split("@")[0]} Telah Keluar Dari Grup Ini,
 contextInfo: {mentionedJid: [...tag], externalAdReply: { thumbnailUrl: ppuser, title: '© Leaving Message', body: '', renderLargerThumbnail: true, sourceUrl: linkgc, mediaType: 1}}})
 }
 if (anu.action == "promote") {
-lubyz.sendMessage(anu.id, {text: `@${anu.author.split("@")[0]} Telah Menjadikan @${num.split("@")[0]} Sebagai Admin Grup Ini`, 
+lubyz.sendMessage(anu.id, {text: @${anu.author.split("@")[0]} Telah Menjadikan @${num.split("@")[0]} Sebagai Admin Grup Ini,
 contextInfo: {mentionedJid: [...tag], externalAdReply: { thumbnailUrl: ppuser, title: '© Promote Message', body: '', renderLargerThumbnail: true, sourceUrl: linkgc, mediaType: 1}}})
 }
 if (anu.action == "demote") {
-lubyz.sendMessage(anu.id, {text: `@${anu.author.split("@")[0]} Telah Memberhentikan @${num.split("@")[0]} Sebagai Admin Grup Ini`, 
+lubyz.sendMessage(anu.id, {text: @${anu.author.split("@")[0]} Telah Memberhentikan @${num.split("@")[0]} Sebagai Admin Grup Ini,
 contextInfo: {mentionedJid: [...tag], externalAdReply: { thumbnailUrl: ppuser, title: '© Demote Message', body: '', renderLargerThumbnail: true, sourceUrl: linkgc, mediaType: 1}}})
 }
-} 
+}
 } catch (err) {
 console.log(err)
 }})
@@ -184,3 +174,5 @@ startSesi()
 process.on('uncaughtException', function (err) {
 console.log('Caught exception: ', err)
 })
+
+  
